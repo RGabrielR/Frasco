2cb23f2605fc7da690dd5edd230e4ade75391d81
Gabriel Alabi¡ <you@example.com>
2022-03-08 11:44:41 -0300

    add viewport

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ function MyApp({ Component, pageProps }) {
     <title>Frasco</title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'/>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
     </Head>
   <Component {...pageProps} /> 
   </>
